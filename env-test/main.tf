5892ff09cc93c0b283106a57a7383848cf17c648
Ganesh Baral <ganesh.baral@hotmail.co.uk>
2022-10-05 20:21:08 +0100

    initial configuration

--- env-test/main.tf
+++ env-test/main.tf
@@ -7,15 +7,13 @@ provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 }
-
 provider "aws" {
-  region = var.region
+  region = "ap-south-1"
 }
-
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "education-eks-${random_string.suffix.result}"
+  cluster_name = "gbaral-eks-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
