ef483a80de64cfcb253e9c3ec5d4fba23e220020
Ganesh Baral <ganesh.baral@hotmail.co.uk>
2022-10-06 21:55:35 +0100

    clusters made public

--- env-test/eks.tf
+++ env-test/eks.tf
@@ -6,7 +6,7 @@ module "eks" {
   cluster_version = "1.22"    
   cluster_endpoint_private_access = true
   cluster_endpoint_public_access = true
-  cluster_endpoint_public_access_cidrs = var.cluster_endpoint_public_access_cidrs 
+  #cluster_endpoint_public_access_cidrs = var.cluster_endpoint_public_access_cidrs 
 
 
   vpc_id     = module.vpc.vpc_id
